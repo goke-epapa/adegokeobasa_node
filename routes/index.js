64c0052473e3c02f5848cdf7d0566dc23cd4c028
Adegoke Obasa <adegokeobasa@gmail.com>
2016-08-31 06:05:29 +0100

    (Publications): Add publications to homepage

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 var articles = require('../data/articles.json');
+var publications = require('../data/publications.json');
 articles.reverse();
+publications.reverse();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     var data = {
         title: 'Adégòkè Ọbasá - Software Engineer based in Nigeria',
-        'articles': articles
+        articles: articles,
+        publications: publications
     };
     res.render('index', data);
 });
