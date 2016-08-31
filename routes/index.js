af82fb3dbc112bc81c7e6bc16ce3871d4a7ad5a4
Adegoke Obasa <adegokeobasa@gmail.com>
2016-08-31 05:57:49 +0100

    (Fixes): Fix issue with articles reverse and add correct "Ọ"for Obasa

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 var articles = require('../data/articles.json');
+articles.reverse();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     var data = {
-        title: 'Adégòkè Obasá - Software Engineer based in Nigeria',
-        'articles': articles.reverse()
+        title: 'Adégòkè Ọbasá - Software Engineer based in Nigeria',
+        'articles': articles
     };
     res.render('index', data);
 });
